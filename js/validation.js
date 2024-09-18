c555d73a9098f2635868ff67ed0c2930642b26cc
Maxim_Titov <73597552+Maxim-Titov@users.noreply.github.com>
2024-09-18 16:53:02 +0300

    Bootstrap
    Додав bootstrap на сторінку

--- js/validation.js
+++ js/validation.js
@@ -1,53 +1,31 @@
 $(document).ready(function() {
     $('#feedback').on('submit', function(event) {
-        let name_field = $('input[name=name]');
-        let email_field = $('input[name=email]');
-        let subject_field = $('input[name=subject]');
-        let message_field = $('textarea[name=message]');
+        const fields = [
+            { element: $('input[name=name]'), errorMessage: "Це поле має бути заповненим" },
+            { element: $('input[name=email]'), errorMessage: "Це поле має бути заповненим", pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, invalidMessage: "Адреса електронної пошти введена не правильно" },
+            { element: $('input[name=subject]'), errorMessage: "Це поле має бути заповненим" },
+            { element: $('textarea[name=message]'), errorMessage: "Це поле має бути заповненим" },
+        ];
 
-        let email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        let isValid = true;
 
-        if ($(name_field).val() === '') {
-            event.preventDefault();
-
-            $(name_field).addClass('error');
-        } else {
-            $(name_field).removeClass('error');
-        }
-
-        if ($(email_field).val() === '') {
-            event.preventDefault();
-
-            $(email_field).addClass('error');
-        } else {
-            if (!email_pattern.test($(email_field).val())) {
+        fields.forEach(field => {
+            const value = field.element.val().trim();
+            if (value === '') {
+                field.element.addClass('error');
+                isValid = false;
                 event.preventDefault();
-
-                $(email_field).addClass('error');
-                $(email_field).val('');
+            } else if (field.pattern && !field.pattern.test(value)) {
+                field.element.addClass('error');
+                isValid = false;
+                event.preventDefault();
             } else {
-                $(email_field).removeClass('error');
+                field.element.removeClass('error');
             }
-        }
+        });
 
-        if ($(subject_field).val() === '') {
-            event.preventDefault();
-
-            $(subject_field).addClass('error');
-        } else {
-            $(subject_field).removeClass('error');
-        }
-
-        if ($(message_field).val() === '') {
-            event.preventDefault();
-
-            $(message_field).addClass('error');
-        } else {
-            $(message_field).removeClass('error');
-        }
-
-        if ($(name_field).is(!':empty') && $(email_field).is(!':empty') && $(subject_field).is(!':empty') && $(message_field).is(!':empty')) {
+        if (isValid) {
             alert("Your message has been sent");
         }
-    })
-})
+    });
+});
