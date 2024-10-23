22641b0e08355eac01e8bde48ead69985003c13c
Maxim-Titov <tutovmaksum01@gmail.com>
2024-10-23 10:46:36 +0300

    Ваше повідомлення про зміни

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const watch = require('gulp-watch');
 const sass = require('gulp-sass')(require('sass'));
 
 function scssToCss() {
-  return gulp.src('../scss/style.scss')
+  return gulp.src('scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('style.css'))
-    .pipe(gulp.dest('../css/'));
+    .pipe(gulp.dest('css/'));
 }
 
 function watchFiles() {
-    gulp.watch('../scss/**/*.scss', scssToCss);
+    gulp.watch('scss/**/*.scss', scssToCss);
 }
 
 gulp.task('default', gulp.series(scssToCss, watchFiles));
